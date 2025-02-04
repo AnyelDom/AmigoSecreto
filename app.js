9e74839c60c5d7c41812a7c247057a71fd621bad
AnyelDom <104875821+AnyelDom@users.noreply.github.com>
2025-02-04 20:53:29 -0300

    crear función agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 let listaAmigos = [];
 
+function agregarAmigo(){
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+    
+    listaAmigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
